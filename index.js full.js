c1e732e9e0b411cbb9c66051ccafcf0dc1cab5df
Rhys Chin <chinrhys@gmail.com>
2024-10-02 10:10:39 -0400

    comment

---
+++ index.js full.js
@@ -0,0 +1,27 @@
+
+// How to play Globle
+
+// insert interactive globe which you can rotate to see all countries
+
+// have random country be chosen as country player is trying to guess
+
+// insert bar at top which player can type in a country's name
+
+//under bar at top, insert directions "insert name of any country to make your first guess"
+
+//loop
+// have guessed country turn a shade of red on interactive globe, the darker the red the closer the random country is in proximity to guessed country
+
+//loop
+// if player inserts a name which is not a country, under bar at top insert "answer not applicable"
+
+//loop
+// if player inserts a country's name which they have already guessed, under bar at top insert "country has already been guessed"
+
+//loop
+// for each country guessed, under bar at top exclaim whether the country is closer or farther than previous guess in regards to random country player is trying to guess
+
+//loop
+// if player guesses a country connected to the randomly selected country, under bar on top insert "you are adjacent to the answer!"
+
+// once the correct country is guessed, turn correct guessed country the color green and exclaim to player that they have won using a banner stating "you guessed correct!"
