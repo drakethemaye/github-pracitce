f9e61dc1734d0a67a3e27d0a2eae7088aac05142
drakethemaye <chinrhys@gmail.com>
2024-10-02 10:36:28 -0400

    Add files via upload
    football player js

---
+++ footballplayer.js
@@ -0,0 +1,25 @@
+// How to create a football player in a video game
+
+// Properties:
+// name- Max Straudy
+// age- 23 years old
+// height- 6 feet 1 inches
+// alive or dead?-alive
+// Wears football gear- Helmet, shoulder pads, cleats, jersey, tights, mouthguards
+// origin- grew up south of Boston, went to Boston college to play football, got drafted to the New England Patriots with the 3rd pick in the NFL draft
+// Position- running back
+// loved or hated?- extremely loved in New England
+// hair color- black
+// skin color- light brown
+// eye color- light brown
+// body build- muscular but toned
+// ethnicity- Chinese and Spanish
+
+//Events:
+// Run Forward, side to side
+// hurdle
+// spin move
+// Trucking
+// catch ball
+// contested catch
+// celebration
