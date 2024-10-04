190c8802608d95db5273dd934f60b83f8dbf2e3c
Rhys Chin <chinrhys@gmail.com>
2024-10-04 10:19:57 -0400

    first commit

---
+++ firstjsday.js
@@ -0,0 +1,12 @@
+const today = new Date ();
+const currentHour = today.getHours();
+let message = "Hello";
+
+// if it's morning, show  "Good Morning message"
+if (currentHour <= 12){
+    message = "Good morning, beautiful world!";
+} else if (cuurentHour > 12 && currentHour <= 17) { // if it's midday, show "Hello" message}
+    
+
+// if it's midday, show "Hello" message
+// if it's evening, show "Good evening message"
